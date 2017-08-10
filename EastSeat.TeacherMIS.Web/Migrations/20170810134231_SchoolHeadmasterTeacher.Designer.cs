2e58bcec189c215ef0cafe9e8398b11d1dbccefc
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2017-08-10 16:58:52 +0300

    Migration SchoolHeadmasterTeacher

---
+++ EastSeat.TeacherMIS.Web/Migrations/20170810134231_SchoolHeadmasterTeacher.Designer.cs
@@ -0,0 +1,372 @@
+﻿using System;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.EntityFrameworkCore.Infrastructure;
+using Microsoft.EntityFrameworkCore.Metadata;
+using Microsoft.EntityFrameworkCore.Migrations;
+using EastSeat.TeacherMIS.Web.Data;
+
+namespace EastSeat.TeacherMIS.Web.Migrations
+{
+    [DbContext(typeof(ApplicationDbContext))]
+    [Migration("20170810134231_SchoolHeadmasterTeacher")]
+    partial class SchoolHeadmasterTeacher
+    {
+        protected override void BuildTargetModel(ModelBuilder modelBuilder)
+        {
+            modelBuilder
+                .HasAnnotation("ProductVersion", "1.1.2")
+                .HasAnnotation("SqlServer:ValueGenerationStrategy", SqlServerValueGenerationStrategy.IdentityColumn);
+
+            modelBuilder.Entity("EastSeat.TeacherMIS.Web.Models.ApplicationUser", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<int>("AccessFailedCount");
+
+                    b.Property<string>("ConcurrencyStamp")
+                        .IsConcurrencyToken();
+
+                    b.Property<string>("Email")
+                        .HasMaxLength(256);
+
+                    b.Property<bool>("EmailConfirmed");
+
+                    b.Property<bool>("LockoutEnabled");
+
+                    b.Property<DateTimeOffset?>("LockoutEnd");
+
+                    b.Property<string>("NormalizedEmail")
+                        .HasMaxLength(256);
+
+                    b.Property<string>("NormalizedUserName")
+                        .HasMaxLength(256);
+
+                    b.Property<string>("PasswordHash");
+
+                    b.Property<string>("PhoneNumber");
+
+                    b.Property<bool>("PhoneNumberConfirmed");
+
+                    b.Property<string>("SecurityStamp");
+
+                    b.Property<bool>("TwoFactorEnabled");
+
+                    b.Property<string>("UserName")
+                        .HasMaxLength(256);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("NormalizedEmail")
+                        .HasName("EmailIndex");
+
+                    b.HasIndex("NormalizedUserName")
+                        .IsUnique()
+                        .HasName("UserNameIndex");
+
+                    b.ToTable("AspNetUsers");
+                });
+
+            modelBuilder.Entity("EastSeat.TeacherMIS.Web.Models.Headmaster", b =>
+                {
+                    b.Property<Guid>("HeadmasterId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<byte[]>("RowVersion")
+                        .IsConcurrencyToken()
+                        .IsRequired()
+                        .ValueGeneratedOnAddOrUpdate();
+
+                    b.Property<Guid>("SchoolId");
+
+                    b.Property<Guid>("TeacherId");
+
+                    b.Property<Guid?>("TeacherId1");
+
+                    b.HasKey("HeadmasterId");
+
+                    b.HasIndex("SchoolId");
+
+                    b.HasIndex("TeacherId1")
+                        .IsUnique();
+
+                    b.HasIndex("TeacherId", "SchoolId");
+
+                    b.ToTable("Headmaster");
+                });
+
+            modelBuilder.Entity("EastSeat.TeacherMIS.Web.Models.School", b =>
+                {
+                    b.Property<Guid>("SchoolId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("Name")
+                        .IsRequired()
+                        .HasMaxLength(200)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(200)");
+
+                    b.Property<byte[]>("RowVersion")
+                        .IsConcurrencyToken()
+                        .IsRequired()
+                        .ValueGeneratedOnAddOrUpdate();
+
+                    b.HasKey("SchoolId");
+
+                    b.ToTable("School");
+                });
+
+            modelBuilder.Entity("EastSeat.TeacherMIS.Web.Models.Teacher", b =>
+                {
+                    b.Property<Guid>("TeacherId")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ConfirmationEscMinute")
+                        .IsRequired()
+                        .HasMaxLength(200)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(200)");
+
+                    b.Property<string>("CurrentPosition")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(100)");
+
+                    b.Property<string>("CurrentPositionAppMinute")
+                        .IsRequired()
+                        .HasMaxLength(200)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(200)");
+
+                    b.Property<DateTime>("CurrentPositionPostingDate")
+                        .HasAnnotation("SqlServer:ColumnType", "date");
+
+                    b.Property<DateTime>("DateOfBirth")
+                        .HasAnnotation("SqlServer:ColumnType", "date");
+
+                    b.Property<string>("FirstAppEscMinute")
+                        .IsRequired()
+                        .HasMaxLength(200)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(200)");
+
+                    b.Property<DateTime>("FirstProbationAppDate")
+                        .HasAnnotation("SqlServer:ColumnType", "date");
+
+                    b.Property<string>("Fullname")
+                        .IsRequired()
+                        .HasMaxLength(50)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(50)");
+
+                    b.Property<string>("Gender")
+                        .IsRequired()
+                        .HasMaxLength(10)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "char(10)");
+
+                    b.Property<string>("IppsNumber")
+                        .IsRequired()
+                        .HasMaxLength(20)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(20)");
+
+                    b.Property<string>("ProbationAppDesignation")
+                        .IsRequired()
+                        .HasMaxLength(100)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(100)");
+
+                    b.Property<string>("RegistrationNumber")
+                        .IsRequired()
+                        .HasMaxLength(20)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(20)");
+
+                    b.Property<byte[]>("RowVersion")
+                        .IsConcurrencyToken()
+                        .IsRequired()
+                        .ValueGeneratedOnAddOrUpdate();
+
+                    b.Property<Guid>("SchoolId");
+
+                    b.Property<string>("UtsFileNumber")
+                        .IsRequired()
+                        .HasMaxLength(20)
+                        .IsUnicode(false)
+                        .HasAnnotation("SqlServer:ColumnType", "varchar(20)");
+
+                    b.HasKey("TeacherId");
+
+                    b.HasIndex("SchoolId");
+
+                    b.ToTable("Teachers");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole", b =>
+                {
+                    b.Property<string>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ConcurrencyStamp")
+                        .IsConcurrencyToken();
+
+                    b.Property<string>("Name")
+                        .HasMaxLength(256);
+
+                    b.Property<string>("NormalizedName")
+                        .HasMaxLength(256);
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("NormalizedName")
+                        .IsUnique()
+                        .HasName("RoleNameIndex");
+
+                    b.ToTable("AspNetRoles");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRoleClaim<string>", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ClaimType");
+
+                    b.Property<string>("ClaimValue");
+
+                    b.Property<string>("RoleId")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("RoleId");
+
+                    b.ToTable("AspNetRoleClaims");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserClaim<string>", b =>
+                {
+                    b.Property<int>("Id")
+                        .ValueGeneratedOnAdd();
+
+                    b.Property<string>("ClaimType");
+
+                    b.Property<string>("ClaimValue");
+
+                    b.Property<string>("UserId")
+                        .IsRequired();
+
+                    b.HasKey("Id");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("AspNetUserClaims");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserLogin<string>", b =>
+                {
+                    b.Property<string>("LoginProvider");
+
+                    b.Property<string>("ProviderKey");
+
+                    b.Property<string>("ProviderDisplayName");
+
+                    b.Property<string>("UserId")
+                        .IsRequired();
+
+                    b.HasKey("LoginProvider", "ProviderKey");
+
+                    b.HasIndex("UserId");
+
+                    b.ToTable("AspNetUserLogins");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserRole<string>", b =>
+                {
+                    b.Property<string>("UserId");
+
+                    b.Property<string>("RoleId");
+
+                    b.HasKey("UserId", "RoleId");
+
+                    b.HasIndex("RoleId");
+
+                    b.ToTable("AspNetUserRoles");
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserToken<string>", b =>
+                {
+                    b.Property<string>("UserId");
+
+                    b.Property<string>("LoginProvider");
+
+                    b.Property<string>("Name");
+
+                    b.Property<string>("Value");
+
+                    b.HasKey("UserId", "LoginProvider", "Name");
+
+                    b.ToTable("AspNetUserTokens");
+                });
+
+            modelBuilder.Entity("EastSeat.TeacherMIS.Web.Models.Headmaster", b =>
+                {
+                    b.HasOne("EastSeat.TeacherMIS.Web.Models.School", "School")
+                        .WithMany("Headmasters")
+                        .HasForeignKey("SchoolId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("EastSeat.TeacherMIS.Web.Models.Teacher", "Teacher")
+                        .WithOne("Headmaster")
+                        .HasForeignKey("EastSeat.TeacherMIS.Web.Models.Headmaster", "TeacherId1");
+                });
+
+            modelBuilder.Entity("EastSeat.TeacherMIS.Web.Models.Teacher", b =>
+                {
+                    b.HasOne("EastSeat.TeacherMIS.Web.Models.School", "School")
+                        .WithMany("Teachers")
+                        .HasForeignKey("SchoolId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRoleClaim<string>", b =>
+                {
+                    b.HasOne("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole")
+                        .WithMany("Claims")
+                        .HasForeignKey("RoleId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserClaim<string>", b =>
+                {
+                    b.HasOne("EastSeat.TeacherMIS.Web.Models.ApplicationUser")
+                        .WithMany("Claims")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserLogin<string>", b =>
+                {
+                    b.HasOne("EastSeat.TeacherMIS.Web.Models.ApplicationUser")
+                        .WithMany("Logins")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+
+            modelBuilder.Entity("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityUserRole<string>", b =>
+                {
+                    b.HasOne("Microsoft.AspNetCore.Identity.EntityFrameworkCore.IdentityRole")
+                        .WithMany("Users")
+                        .HasForeignKey("RoleId")
+                        .OnDelete(DeleteBehavior.Cascade);
+
+                    b.HasOne("EastSeat.TeacherMIS.Web.Models.ApplicationUser")
+                        .WithMany("Roles")
+                        .HasForeignKey("UserId")
+                        .OnDelete(DeleteBehavior.Cascade);
+                });
+        }
+    }
+}
---
+++ EastSeat.TeacherMIS.Web/Migrations/20170810134231_SchoolHeadmasterTeacher.cs
@@ -0,0 +1,117 @@
+﻿using System;
+using System.Collections.Generic;
+using Microsoft.EntityFrameworkCore.Migrations;
+
+namespace EastSeat.TeacherMIS.Web.Migrations
+{
+    public partial class SchoolHeadmasterTeacher : Migration
+    {
+        protected override void Up(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.CreateTable(
+                name: "School",
+                columns: table => new
+                {
+                    SchoolId = table.Column<Guid>(nullable: false),
+                    Name = table.Column<string>(type: "varchar(200)", unicode: false, maxLength: 200, nullable: false),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_School", x => x.SchoolId);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Teachers",
+                columns: table => new
+                {
+                    TeacherId = table.Column<Guid>(nullable: false),
+                    ConfirmationEscMinute = table.Column<string>(type: "varchar(200)", unicode: false, maxLength: 200, nullable: false),
+                    CurrentPosition = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    CurrentPositionAppMinute = table.Column<string>(type: "varchar(200)", unicode: false, maxLength: 200, nullable: false),
+                    CurrentPositionPostingDate = table.Column<DateTime>(type: "date", nullable: false),
+                    DateOfBirth = table.Column<DateTime>(type: "date", nullable: false),
+                    FirstAppEscMinute = table.Column<string>(type: "varchar(200)", unicode: false, maxLength: 200, nullable: false),
+                    FirstProbationAppDate = table.Column<DateTime>(type: "date", nullable: false),
+                    Fullname = table.Column<string>(type: "varchar(50)", unicode: false, maxLength: 50, nullable: false),
+                    Gender = table.Column<string>(type: "char(10)", unicode: false, maxLength: 10, nullable: false),
+                    IppsNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    ProbationAppDesignation = table.Column<string>(type: "varchar(100)", unicode: false, maxLength: 100, nullable: false),
+                    RegistrationNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: false),
+                    SchoolId = table.Column<Guid>(nullable: false),
+                    UtsFileNumber = table.Column<string>(type: "varchar(20)", unicode: false, maxLength: 20, nullable: false)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Teachers", x => x.TeacherId);
+                    table.ForeignKey(
+                        name: "FK_Teachers_School_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "School",
+                        principalColumn: "SchoolId",
+                        onDelete: ReferentialAction.Cascade);
+                });
+
+            migrationBuilder.CreateTable(
+                name: "Headmaster",
+                columns: table => new
+                {
+                    HeadmasterId = table.Column<Guid>(nullable: false),
+                    RowVersion = table.Column<byte[]>(rowVersion: true, nullable: false),
+                    SchoolId = table.Column<Guid>(nullable: false),
+                    TeacherId = table.Column<Guid>(nullable: false),
+                    TeacherId1 = table.Column<Guid>(nullable: true)
+                },
+                constraints: table =>
+                {
+                    table.PrimaryKey("PK_Headmaster", x => x.HeadmasterId);
+                    table.ForeignKey(
+                        name: "FK_Headmaster_School_SchoolId",
+                        column: x => x.SchoolId,
+                        principalTable: "School",
+                        principalColumn: "SchoolId",
+                        onDelete: ReferentialAction.Cascade);
+                    table.ForeignKey(
+                        name: "FK_Headmaster_Teachers_TeacherId1",
+                        column: x => x.TeacherId1,
+                        principalTable: "Teachers",
+                        principalColumn: "TeacherId",
+                        onDelete: ReferentialAction.Restrict);
+                });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Headmaster_SchoolId",
+                table: "Headmaster",
+                column: "SchoolId");
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Headmaster_TeacherId1",
+                table: "Headmaster",
+                column: "TeacherId1",
+                unique: true);
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Headmaster_TeacherId_SchoolId",
+                table: "Headmaster",
+                columns: new[] { "TeacherId", "SchoolId" });
+
+            migrationBuilder.CreateIndex(
+                name: "IX_Teachers_SchoolId",
+                table: "Teachers",
+                column: "SchoolId");
+        }
+
+        protected override void Down(MigrationBuilder migrationBuilder)
+        {
+            migrationBuilder.DropTable(
+                name: "Headmaster");
+
+            migrationBuilder.DropTable(
+                name: "Teachers");
+
+            migrationBuilder.DropTable(
+                name: "School");
+        }
+    }
+}
