21dcf69e1795e3eb6dddbdf90889c53dd889fc97
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2017-08-11 17:38:31 +0300

    Teachers controller unit tests setup

---
+++ EastSeat.TeacherMIS.Web.Tests/Controllers/TeachersControllerUnitTests.cs
@@ -0,0 +1,20 @@
+using System;
+using Xunit;
+
+namespace EastSeat.TeacherMIS.Web.Tests.Controllers
+{
+    public class TeachersControllerUnitTests
+    {
+        [Fact]
+        [Trait("Controller", "Teachers")]
+        public void Index_Returns_ListOfTeacherViewModel()
+        {
+            //Given
+            
+            //When
+            
+            
+            //Then
+        }
+    }
+}
