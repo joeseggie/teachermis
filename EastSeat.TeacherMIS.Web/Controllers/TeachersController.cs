609814ed4d5d8f37353d17f6866147b22e0a9c3e
Joseph Serunjogi <joseph.serunjogi@hotmail.com>
2017-08-11 10:36:03 +0300

    Teachers controller school, subject and file action methods.

--- EastSeat.TeacherMIS.Web/Controllers/TeachersController.cs
+++ EastSeat.TeacherMIS.Web/Controllers/TeachersController.cs
@@ -94,12 +94,72 @@ namespace EastSeat.TeacherMIS.Web.Controllers
 
         public IActionResult School(string id, string search, int? page)
         {
-            return View();
+            if(!string.IsNullOrWhiteSpace(id))
+            {
+                Guid schoolId;
+
+                if (Guid.TryParse(id, out schoolId))
+                {
+                    var model = _db.Teachers
+                        .Where(t => t.SchoolId == schoolId)
+                        .Select(t => new TeacherViewModel{
+                            TeacherId = t.TeacherId,
+                            ConfirmationEscMinute = t.ConfirmationEscMinute,
+                            CurrentPosition = t.CurrentPosition,
+                            CurrentPositionAppMinute = t.CurrentPositionAppMinute,
+                            CurrentPositionPostingDate = t.CurrentPositionPostingDate,
+                            DateOfBirth = t.DateOfBirth,
+                            FirstAppEscMinute = t.FirstAppEscMinute,
+                            FirstProbationAppDate = t.FirstProbationAppDate,
+                            Fullname = t.Fullname,
+                            Gender = t.Gender,
+                            IppsNumber = t.IppsNumber,
+                            ProbationAppDesignation = t.ProbationAppDesignation,
+                            RegistrationNumber = t.RegistrationNumber,
+                            RowVersion = t.RowVersion,
+                            SchoolId = t.SchoolId,
+                            UtsFileNumber = t.UtsFileNumber
+                        }).ToList();
+
+                    return View(model);
+                }
+            }
+            return View("SchoolNotFound");
         }
 
         public IActionResult Subject(string id, string search, int? page)
         {
-            return View();
+            if(!string.IsNullOrWhiteSpace(id))
+            {
+                Guid subjectId;
+
+                if (Guid.TryParse(id, out subjectId))
+                {
+                    var model = _db.SubjectsTaught
+                        .Where(t => t.SubjectId == subjectId)
+                        .Select(t => new TeacherViewModel{
+                            TeacherId                  = t.Teacher.TeacherId,
+                            ConfirmationEscMinute      = t.Teacher.ConfirmationEscMinute,
+                            CurrentPosition            = t.Teacher.CurrentPosition,
+                            CurrentPositionAppMinute   = t.Teacher.CurrentPositionAppMinute,
+                            CurrentPositionPostingDate = t.Teacher.CurrentPositionPostingDate,
+                            DateOfBirth                = t.Teacher.DateOfBirth,
+                            FirstAppEscMinute          = t.Teacher.FirstAppEscMinute,
+                            FirstProbationAppDate      = t.Teacher.FirstProbationAppDate,
+                            Fullname                   = t.Teacher.Fullname,
+                            Gender                     = t.Teacher.Gender,
+                            IppsNumber                 = t.Teacher.IppsNumber,
+                            ProbationAppDesignation    = t.Teacher.ProbationAppDesignation,
+                            RegistrationNumber         = t.Teacher.RegistrationNumber,
+                            RowVersion                 = t.Teacher.RowVersion,
+                            SchoolId                   = t.Teacher.SchoolId,
+                            UtsFileNumber              = t.Teacher.UtsFileNumber
+                        }).ToList();
+
+                    return View(model);
+                }
+            }
+            return View("SubjectNotFound");
         }
     }
 }
